35b6722b12fa5463967a014330493b071fb513c4
Wojciech Klupś <wojciech.klups@gmail.com>
2019-06-30 11:58:16 +0200

    Created car testing implementation

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
+import model.Car;
+import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World hi!");
+        System.out.println("Hi! Please enter year of production and model of the car");
+        Scanner scan = new Scanner(System.in);
+        Car car = new Car(scan.nextInt(), scan.nextLine());
+        System.out.println("New car created! " + car.getYearOfProduction() + car.getModel());
+        car.accelerate();
+        System.out.println("The current speed is " + car.getVelocity());
     }
 }
