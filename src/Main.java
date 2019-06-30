ad2991244d0b81c37df5a347c06e802cb94c9244
Błażej Ślachetka <blazej.slachetka@gmail.com>
2019-06-30 12:13:58 +0200

    Test przyspieszenia

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,23 @@
 public class Main {
 
     public static void main(String[] args) {
-        
+
         System.out.println("przyspiesz");
 
-        for (int i = 0; i <=5 ; i++) {
-            ;
+
+        
+        for (int i = 0; i == 5; i++) {
+            car.accelerate();
             System.out.println("The current speed is" + " " + i);
         }
 
         System.out.println("hamuj");
 
-        for (int y = 5; y >= 0 ; y--) {
+        for (int y = 5; y >= 0; y--) {
+            car.decrease();
             System.out.println("The current speed is" + " " + y);
 
         }
-
-
 
 
     }
